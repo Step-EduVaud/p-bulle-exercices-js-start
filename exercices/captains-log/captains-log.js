0015045f35c6401a358622f1da0e1ebf7a66d5de
Stepan <pl77sbr@eduvaud.ch>
2025-12-05 14:41:08 +0100

    feat(captain-log): Terminer l'exercice;
    [40] [Done]

--- exercices/captains-log/captains-log.js
+++ exercices/captains-log/captains-log.js
@@ -6,9 +6,9 @@
  * @returns {string} the generated registry number.
  */
 export function randomShipRegistryNumber() {
-  throw new Error(
-    'Please remove this line and implement the randomShipRegistryNumber() function',
-  );
+  const min = 1000
+  const max = 9999
+  return "NCC-" + Math.round(Math.random() * (max-min) + min)
 }
 
 /**
@@ -17,9 +17,9 @@ export function randomShipRegistryNumber() {
  * @returns {number} a stardate between 41000 (inclusive) and 42000 (exclusive).
  */
 export function randomStardate() {
-  throw new Error(
-    'Please remove this line and implement the randomStardate() function',
-  );
+  const min = 41000.0
+  const max = 42000.0
+  return Math.random() * (max - min) + min
 }
 
 /**
@@ -28,7 +28,6 @@ export function randomStardate() {
  * @returns {string} a one-letter planet class.
  */
 export function randomPlanetClass() {
-  throw new Error(
-    'Please remove this line and implement the randomStardate() function',
-  );
+  var classPlanet = ['D','H','J','K','L','M','N','R','T','Y'];
+  return classPlanet[Math.floor(Math.random() * (classPlanet.length - 0) + 0)]
 }
