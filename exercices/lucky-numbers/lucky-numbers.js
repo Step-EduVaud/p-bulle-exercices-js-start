f7efcc6a6c64e653657e8478cdd93189a57b456b
Stepan <pl77sbr@eduvaud.ch>
2025-12-12 15:47:20 +0100

    feat(lucky-numbers): Terminer l'exercice;
    [30] [Done]

--- exercices/lucky-numbers/lucky-numbers.js
+++ exercices/lucky-numbers/lucky-numbers.js
@@ -8,7 +8,7 @@
  * @returns {number} sum of the two arrays
  */
 export function twoSum(array1, array2) {
-  throw new Error('Remove this line and implement the function');
+  return Number(array1.join('')) + Number(array2.join(''));
 }
 
 /**
@@ -18,7 +18,7 @@ export function twoSum(array1, array2) {
  * @returns {boolean} whether the number is a palindrome or not
  */
 export function luckyNumber(value) {
-  throw new Error('Remove this line and implement the function');
+  return String(value) === String(value).split('').reverse().join('')
 }
 
 /**
@@ -29,5 +29,10 @@ export function luckyNumber(value) {
  * @returns {string} error message
  */
 export function errorMessage(input) {
-  throw new Error('Remove this line and implement the function');
+  if (!Boolean(input))
+    return 'Required field'
+  else if (!Number(input) || Number(input) === 0)
+    return 'Must be a number besides 0'
+  else
+    return ''
 }
