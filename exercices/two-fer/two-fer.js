b4a540d5abd6ae7c23c32ba8baabad23af7dd52f
Stepan <pl77sbr@eduvaud.ch>
2025-12-19 15:05:52 +0100

    feat(two-fer): Terminer l'exercice;
    [30] [Done]

--- exercices/two-fer/two-fer.js
+++ exercices/two-fer/two-fer.js
@@ -3,6 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-export const twoFer = () => {
-  throw new Error('Remove this line and implement the function');
+export const twoFer = (name = `you`) => {
+  return `One for ${name}, one for me.`
 };
