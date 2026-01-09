e63b8be0bcadb45a8dcc4560b04eae4e3df63070
Stepan <pl77sbr@eduvaud.ch>
2026-01-09 11:12:30 +0100

    feat(linked-list): Terminer l'exercice;
    [110] [Done]

--- exercices/linked-list/linked-list.js
+++ exercices/linked-list/linked-list.js
@@ -4,27 +4,65 @@
 //
 
 export class LinkedList {
-  push() {
-    throw new Error('Remove this line and implement the function');
-  }
+    constructor() {
+        this.tab = [];
+    }
+    push(numberValue) {
+        this.tab[this.tab.length] = numberValue;
+        return this.tab.length;
+    }
 
-  pop() {
-    throw new Error('Remove this line and implement the function');
-  }
+    pop() {
+        if (this.tab.length === 0)
+            return undefined;
 
-  shift() {
-    throw new Error('Remove this line and implement the function');
-  }
+        const result = this.tab[this.tab.length - 1];
 
-  unshift() {
-    throw new Error('Remove this line and implement the function');
-  }
+        this.tab.length = this.tab.length - 1;
+        return result;
+    }
 
-  delete() {
-    throw new Error('Remove this line and implement the function');
-  }
 
-  count() {
-    throw new Error('Remove this line and implement the function');
-  }
+    shift() {
+        if (this.tab.length === 0)
+            return undefined;
+
+        const result = this.tab[0];
+
+        for (let i = 1; i < this.tab.length; i++) {
+            this.tab[i - 1] = this.tab[i];
+        }
+
+        this.tab.splice(this.tab.length - 1);
+        return result;
+    }
+
+    unshift(...numberValue) {
+        for (let i = this.tab.length; i >= 0; i--) {
+            this.tab[i + numberValue.length - 1] = this.tab[i - 1];
+        }
+        for (let i = 0; i < numberValue.length; i++) {
+            this.tab[i] = numberValue[i];
+        }
+        return this.tab.length;
+    }
+
+    delete(numberValue) {
+        let indexResult = this.tab.indexOf(numberValue);
+        if (indexResult === -1)
+            return undefined;
+        for (let i = indexResult + 1; i < this.tab.length; i++) {
+            this.tab[i - 1] = this.tab[i];
+        }
+        this.tab.length = this.tab.length - 1;
+        return numberValue;
+    }
+
+    count() {
+        let numberCount = 0;
+        for (let i = 0; i < this.tab.length; i++) {
+            numberCount++;
+        }
+        return numberCount;
+    }
 }
